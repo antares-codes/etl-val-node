3338fb0c8918776fd41c9adbd43a71cc550ed9da
Antares <44238787+antares-etherlite@users.noreply.github.com>
2021-10-19 12:06:06 +0400

    Update watchdognode.sh

--- cron/watchdognode.sh
+++ cron/watchdognode.sh
@@ -3,5 +3,5 @@ if pgrep openethereum >/dev/null
 then
 	echo "Process is running."
 else
-	cd /root/validator-node-dockerized/ && screen -m -d -S node ./openethereum --config=node.toml --jsonrpc-port=8545 --jsonrpc-cors=all --jsonrpc-interface=all --jsonrpc-hosts=all --jsonrpc-apis=web3,eth,net,parity --ws-interface=all --ws-apis=web3,eth,net,parity,pubsub --ws-origins=all --ws-hosts=all --ws-max-connections=10 --max-peers=100
+	cd /root/validator-node-screen/ && screen -m -d -S node ./openethereum --config=node.toml --jsonrpc-port=8545 --jsonrpc-cors=all --jsonrpc-interface=all --jsonrpc-hosts=all --jsonrpc-apis=web3,eth,net,parity --ws-interface=all --ws-apis=web3,eth,net,parity,pubsub --ws-origins=all --ws-hosts=all --ws-max-connections=10 --max-peers=100
 fi
