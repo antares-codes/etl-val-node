20d57f76fe32739b54bb0119299480ef8e75bbcb
Antares <44238787+antares-etherlite@users.noreply.github.com>
2022-02-18 22:15:58 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -107,6 +107,6 @@ echo "Start Node..."
 screen -m -d -S node ./openethereum --config=node.toml --jsonrpc-port=8545 --jsonrpc-cors=all --jsonrpc-interface=all --jsonrpc-hosts=all --jsonrpc-apis=web3,eth,net,parity --ws-interface=all --ws-apis=web3,eth,net,parity,pubsub --ws-origins=all --ws-hosts=all --ws-max-connections=5 --max-peers=30
 
 # Reboot the server
-# del chain.zip
+# rm -v chains.zip
 #reboot
 
